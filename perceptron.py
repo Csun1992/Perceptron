f0f392fd9c8138e9cdb9a7e79e1996d4b90c44c6
Csun1992 <sonshicyu@gmail.com>
2018-08-13 01:13:14 -0500

    Add training data

--- perceptron.py
+++ perceptron.py
@@ -6,6 +6,20 @@ def sign(x):
     else:
         return np.sign(x)
 
+def findLine(point1, point2):
+    slope = (point2[1]-point1[1])/float(point2[0]-point1[0])
+    intercept = point2[1] - slope*point2[0]
+    return slope, intercept
+
+
+def classify(data, slope, intercept):
+    group = []
+    size = np.size(data, 0)
+    for i in range(size):
+        group.append(sign(data[i,1]-slope*data[i,0]-intercept))
+    return group
+
+
 class Pla:
     def __init__(self, data, group):
         self.size = np.size(data, 0) 
@@ -15,16 +29,31 @@ class Pla:
         self.coeff = np.zeros((np.size(data, 1)+1, 1))
         self.classifiedGroup = [0] * self.size
          
-    def updateMisclassified():
+    def updateMisclassified(self):
         self.misclassified = []
         for i in range(self.size):
            if(self.classifiedGroup[i] != self.label[i]):
               self.misclassified.append(i) 
     
-    def pla():
+    def pla(self):
+        repeat = 0
         while not self.misclassified:
+            repeat += 1
             index = self.misclassified[0]
             self.coeff = self.coeff + self.correctGroup[index]*self.data[index, :]
             for i in range(self.size):
                 self.classifiedGroup[i] = sign(self.coeff.dot(self.data[i, :]))
                 self.updateMisclassified()
+        return repeat
+
+
+if __name__ == "__main__":
+    data = np.random.uniform(-1, 1, 20).reshape(10, 2)
+    x1 = data[0, :]
+    x2 = data[1, :]
+    slope, intercept = findLine(x1, x2)
+    group = classify(data, slope, intercept)
+
+    perceptron = Pla(data, group)
+    repeat = perceptron.pla()
+    print repeat
